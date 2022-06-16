cad75e71db46a55c154c90cd6a6b7bdae2dd925a
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-06-16 13:26:35 -0500

    add list slice. Co-authored-by: Pauline314159 fagie37@hotmail.com

--- client/src/lib/Store.js
+++ client/src/lib/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import boardsReducer from "../features/boards/boards";
+import listReducer from "../features/lists";
 
 const store = configureStore({
   reducer: {
     boards: boardsReducer,
+    lists: listReducer,
   },
 });
 
