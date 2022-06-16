75bde4c113674f7e1963cca5be06d13b7c01798e
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-06-16 10:32:31 -0500

    add list schema

---
+++ server/models/list.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ListSchema = new Schema({
+      title: {
+        type: String
+      },
+      boardId: {
+        type: Schema.Types.ObjectId,
+      },
+      position: {
+        type: Number,
+      },
+      cards: [
+        {type: Schema.Types.ObjectId,
+        ref: 'Card'}
+      ],
+})
+
+const List = mongoose.model('List', ListSchema);
+
+module.exports = List;
