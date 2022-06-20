35b8b5ebe22c68fd9ce3d1582ff833f6ae3c876f
Pauline3414159 <fagie37@hotmail.com>
2022-06-20 11:56:23 -0700

    add placeholder onclick for NewList component. Co-authored-by: olgashi oshiryaeva@protonmail.com

--- client/src/components/boards/NewList.js
+++ client/src/components/boards/NewList.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const NewList = () => {
+  // TODO: change the className when element is clicked
+  const [formVisible, setFormVisible] = useState(false)
   return (
-    <div id="new-list" className="new-list">
+    <div id="new-list" className="new-list" onClick={(e) => console.log('we clicked the element')}>
       <span>Add a list...</span>
       <input type="text" placeholder="Add a list..." />
       <div>
