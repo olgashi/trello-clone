5de96d863ea2e4a852ea9178ffb98f50ac677d13
Pauline3414159 <fagie37@hotmail.com>
2022-06-21 08:20:27 -0700

    add control for NewList form. Co-authored-by: olgashi oshiryaeva@protonmail.com

--- client/src/components/boards/NewList.js
+++ client/src/components/boards/NewList.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 
 const NewList = () => {
-  const [formVisible, setFormVisible] = useState(false)
+  const [formVisible, setFormVisible] = useState(false);
+  const [newListTitle, setNewListTitle] = useState("");
+
   const handleNewListClick = () => {
     setFormVisible(true);
   };
@@ -15,10 +17,14 @@ const NewList = () => {
     setFormVisible(false);
   };
 
+  const handleNewTitleChange = (e) => {
+    setNewListTitle(e.target.value);
+  }
+
   return (
     <div id="new-list" className={`new-list ${formVisible ? "selected" : ""}`} onClick={handleNewListClick}>
       <span>Add a list...</span>
-      <input type="text" placeholder="Add a list..." />
+      <input type="text" placeholder="Add a list..." onChange={handleNewTitleChange} value={newListTitle}/>
       <div>
         <input type="submit" className="button" value="Save" onClick={handleSaveNewListClick} />
         <i className="x-icon icon" onClick={handleXOutNewListClick}></i>
