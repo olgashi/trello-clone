ab7ab1cc5fb134a12fe96367292539f7e472f621
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-06-22 12:57:49 -0500

    Add post api/cards endpoint and route handler. Co-authored-by: Pauline3414159 fagie37@hotmail.com

--- server/controllers/cardsController.js
+++ server/controllers/cardsController.js
@@ -1,4 +1,5 @@
 const Card = require("../models/card");
+const List = require("../models/list");
 const HttpError = require("../models/httpError");
 const { validationResult } = require("express-validator");
 const mongoose = require("mongoose");
@@ -21,4 +22,37 @@ const getCardById = (req, res, next) => {
   }
 }
 
+const createCard = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    Card.create({...req.body.card, listId : req.body.listId})
+    .then(card => {
+      req.card = card;
+      next();
+    })
+    .catch(err => {
+      next(new HttpError("Creating card failed, please try again", 500));
+    })
+
+  } else {
+    return next(new HttpError("The title or listId is empty", 404));
+  }
+}
+
+const addCardToList = (req, res, next) =>{
+  const card = req.card;
+
+  List.findOneAndUpdate(
+    { _id: card.listId },
+    { $push: {cards: card._id} }
+    )
+    .then(_ => res.json(card))
+    .catch(error => 
+      next(new HttpError("Adding card to list failed, please try again", 500))
+      )
+}
+
 exports.getCardById = getCardById;
+exports.createCard = createCard;
+exports.addCardToList = addCardToList;
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const boardsController = require("../controllers/boardsController");
 const listsController = require("../controllers/listsController");
 const cardsController = require("../controllers/cardsController");
-const { validateBoard, validateList, validateListTitle } = require("../validators/validators");
+const { validateBoard, validateList, validateListTitle, validateCard } = require("../validators/validators");
 
 router.get("/boards", boardsController.getBoards);
 router.get('/boards/:id', boardsController.getBoardById);
@@ -11,5 +11,6 @@ router.post("/boards", validateBoard, boardsController.createBoard);
 router.put("/lists/:id", validateListTitle, listsController.updateListTitle);
 router.post("/lists", validateList, listsController.createList, listsController.addListToBoard);
 router.get("/cards/:id", cardsController.getCardById);
+router.post("/cards", validateCard, cardsController.createCard, cardsController.addCardToList);
 
 module.exports = router;
