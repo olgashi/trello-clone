e8d180574bdefd7b213c4ec7fa074528e7644c92
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-06-22 10:39:36 -0500

    controlled ListTitleInput component. Co-authored-by: Pauline3414159

--- client/src/components/boards/ListTitleInput.js
+++ client/src/components/boards/ListTitleInput.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ListTitleInput = ({ title }) => {
+  const [currentTitle, setCurrentTitle] = useState(title)
+  const changeHandler = (e) => setCurrentTitle(e.target.value)
   return (
-    <input className="list-title" placeholder={title} value={title}></input>
+    <input className="list-title"  value={currentTitle} onChange={changeHandler}></input>
   )
 }
 
