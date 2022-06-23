949dadb1b6d26215751c649fc71b74f8af3e6adb
Pauline3414159 <fagie37@hotmail.com>
2022-06-23 09:35:31 -0700

    add state to addCardForm. Co-authored-by: olgashi oshiryaeva@protonmail.com

--- client/src/components/boards/AddCardForm.js
+++ client/src/components/boards/AddCardForm.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 const AddCardForm = () => {
+  const [cardTitle, setCardTitle] = useState('')
+  const handleOnChange = (e) => setCardTitle(e.target.value)
   return (
     <div className="add-dropdown add-bottom active-card">
     <div className="card">
       <div className="card-info"></div>
-      <textarea name="add-card"></textarea>
+      <textarea name="add-card" onChange={handleOnChange}></textarea>
       <div className="members"></div>
     </div>
     <a className="button">Add</a>
