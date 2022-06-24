1a96982adc1c985093bb8be15838f157f21ae463
Pauline3414159 <fagie37@hotmail.com>
2022-06-24 11:16:26 -0700

    add post /api/comments endpoint. Co-authored-by: olgashi oshiryaeva@protonmail.com

---
+++ server/controllers/commentsController.js
@@ -0,0 +1,44 @@
+const Comment = require("../models/comment");
+const Card = require("../models/card")
+const HttpError = require("../models/httpError");
+const { validationResult } = require("express-validator");
+
+const createComment = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    Comment.create(
+      { ...req.body.comment, cardId: req.body.cardId }
+    ).then(comment => {
+      req.comment = comment;
+      next();
+    }).catch(err => {
+      next(new HttpError("creating comment failed, please try again", 500));
+    })
+  } else {
+    return next(new HttpError("the comment text or cardId is empty", 404))
+  }
+}
+
+const addCommentToCard = (req, res, next) => {
+  const comment = req.comment;
+  Card.findOneAndUpdate(
+    { _id: comment.cardId },
+    { $push: { comments: comment._id } }
+  ).then(_ => res.json(comment))
+    .catch(_ =>
+      next(new HttpError("Adding comment to card failed, please try again", 500))
+    )
+
+}
+
+exports.createComment = createComment;
+exports.addCommentToCard = addCommentToCard;
+
+/**
+ * {
+  "cardId": 9,
+  "comment": {
+    "text": "This is my comment"
+  }
+}
+ */
--- server/validators/validators.js
+++ server/validators/validators.js
@@ -4,4 +4,4 @@ exports.validateBoard = [check("board.title").not().isEmpty()];
 exports.validateList = [check("boardId").not().isEmpty(), check("list.title").not().isEmpty()];
 exports.validateListTitle = [check("title").not().isEmpty()];
 exports.validateCard = [check("listId").not().isEmpty(), check("card.title").not().isEmpty()];
-
+exports.validateComment = [check("cardId").not().isEmpty(), check("comment.text").not().isEmpty()]; 
